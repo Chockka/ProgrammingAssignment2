ef703576b92d1f30de5abc5195d16ece9cb71816
Chockka <chockkalingam@ymail.com>
2016-05-15 11:08:36 -0500

    R programming Week 3 - Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - Week 3 # programming assignment
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix 
+## rather than computing it repeatedly
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+## Sample run
+
+## > x <- diag(6,3)
+## > x
+##      [,1] [,2] [,3]
+##[1,]    6    0    0
+##[2,]    0    6    0
+##[3,]    0    0    6
+
+##> CachedMatrix <- makeCacheMatrix(x)
+##> cacheSolve(CachedMatrix)
+
+##           [,1]      [,2]      [,3]
+## [1,] 0.1666667 0.0000000 0.0000000
+## [2,] 0.0000000 0.1666667 0.0000000
+## [3,] 0.0000000 0.0000000 0.1666667
+
+## > cacheSolve(CachedMatrix)
+## getting cached data
+##           [,1]      [,2]      [,3]
+## [1,] 0.1666667 0.0000000 0.0000000
+## [2,] 0.0000000 0.1666667 0.0000000
+## [3,] 0.0000000 0.0000000 0.1666667
+
+## End of Assignment 
+
